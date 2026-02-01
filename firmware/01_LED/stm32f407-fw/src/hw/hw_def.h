68148dce6ac625460ff29a85c8b77ba14b2067a3
Hancheol Cho <chcbaram@gmail.com>
2026-02-01 18:31:26 +0900

    LED 프로젝트 추가

---
+++ firmware/01_LED/stm32f407-fw/src/hw/hw_def.h
@@ -0,0 +1,27 @@
+#ifndef HW_DEF_H_
+#define HW_DEF_H_
+
+
+#include "bsp.h"
+
+
+#define _DEF_FIRMWATRE_VERSION      "V260201R1"
+#define _DEF_BOARD_NAME             "STM32F407-FW"
+
+
+
+
+#define _USE_HW_RTOS
+
+
+#define _USE_HW_LED
+#define      HW_LED_MAX_CH          3
+
+
+
+//-- CLI
+//
+#define _USE_CLI_HW_UART            1
+
+
+#endif
